f8792771ae6cc65bad66ab372faabbd245c60dde
Suhas Siriprole <suhas.siriprole@gmail.com>
2021-10-13 18:02:44 +0530

    fixes and add readme

---
+++ README.md
@@ -0,0 +1,5 @@
+# CRWN-Clothing
+
+This is Sample E-commerce App built using React.js, complete with Redux, Hooks, Stripe, Firebase & Styled Components.
+
+Yet to Integrate - GraphQL & ContextAPI.
--- src/redux/store.js
+++ src/redux/store.js
@@ -3,11 +3,12 @@ import logger from "redux-logger";
 import { persistStore } from "redux-persist";
 
 import rootReducer from "./root-reducer";
+import thunk from "redux-thunk";
 
-const middlewares = [];
+const middlewares = [thunk];
 
-if (process.env.NODE_ENV === 'development') {
-    middlewares.push(logger)
+if (process.env.NODE_ENV === "development") {
+	middlewares.push(logger);
 }
 
 const store = createStore(rootReducer, applyMiddleware(...middlewares));
